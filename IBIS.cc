183ae9addcc40e28298f952c081095c8f33e3bd5
Evgeny Shirokonosov <eshirokonosov@gmail.com>
2022-01-27 18:19:47 +0300

    fix: free for FileOrGz buf

--- IBIS.cc
+++ IBIS.cc
@@ -208,7 +208,7 @@ size_t FileOrGZ<gzFile>::pfwrite(const void *ptr, size_t size, size_t n) {      
 template<>
 int FileOrGZ<FILE *>::close() {
 	assert(buf_len == 0);
-	// should free buf, but I know the program is about to end, so won't
+	free(buf);
 	return fclose(fp);
 }
 
@@ -216,6 +216,7 @@ template<>
 int FileOrGZ<gzFile>::close() {
 	if (buf_len > 0)
 		gzwrite(fp, buf, buf_len);
+	free(buf);
 	return gzclose(fp);
 }
 
